94bb872ffaaee2ce269fe98b4152126c83ba56fd
Christina Whethers <flandresamarawr@gmail.com>
2022-05-05 00:12:47 -0400

    Add initial template to add React later

--- src/main/java/com/tunesio/tunesio/controllers/SongController.java
+++ src/main/java/com/tunesio/tunesio/controllers/SongController.java
@@ -1,4 +1,13 @@
 package com.tunesio.tunesio.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SongController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
 }
