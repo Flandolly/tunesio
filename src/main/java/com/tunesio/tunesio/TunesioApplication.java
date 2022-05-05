91453e3503c386aa121b948b0dbdb99a6b123577
Christina Whethers <flandresamarawr@gmail.com>
2022-05-04 21:09:14 -0400

    Initial commit

---
+++ src/main/java/com/tunesio/tunesio/TunesioApplication.java
@@ -0,0 +1,13 @@
+package com.tunesio.tunesio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunesioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunesioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tunesio/tunesio/TunesioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunesio.tunesio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunesioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
