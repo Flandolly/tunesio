a22c3773b99dd4e0332d2096261d6bab02242934
Christina Whethers <flandresamarawr@gmail.com>
2022-05-08 12:48:23 -0400

    Add CORS configuration

--- src/main/java/com/tunesio/tunesio/TunesioApplication.java
+++ src/main/java/com/tunesio/tunesio/TunesioApplication.java
@@ -2,9 +2,26 @@ package com.tunesio.tunesio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
 public class TunesioApplication {
+
+	@Configuration
+	public static class CorsSetup extends WebMvcConfigurationSupport {
+
+		@Override
+		protected void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+					.allowedOrigins("*")
+					.allowedHeaders("*")
+					.maxAge(-1)
+					.allowCredentials(false);
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TunesioApplication.class, args);
